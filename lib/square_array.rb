8ed3e24e5aad0d1c9c0b153cd9a11b9fbf7d5a07
Barak Saidoff <barak.saidoff@gmail.com>
2019-09-18 11:11:04 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,16 +1,9 @@
-# def square_array(array)
-#   i=0
-#   while i < array.length do
-#     Math.sqrt(array[i])
-#     i +=1
-#   end
-# end
-
 def square_array(array)
     i=0
     while i < array.length do
-      puts array[i] ** array[i]
+      array[i] *= array[i]
       i +=1
     end
+    return puts array
   end
-
+  
